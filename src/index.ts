aa89ecce8eb40808b152842dfcbb596741e86f06
kuuote <znmxodq1@gmail.com>
2021-12-01 13:39:04 +0000

    供養

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,40 @@
-import { CompleteResult, events, ExtensionContext, sources, VimCompleteItem, workspace } from 'coc.nvim';
+import {events, ExtensionContext, languages, VimCompleteItem, workspace, CancellationToken, CompletionContext, Position, TextDocument, CompletionList, CompletionItem, Range} from 'coc.nvim';
 
 export const activate = async (context: ExtensionContext): Promise<void> => {
   context.subscriptions.push(
-    sources.createSource({
-      name: 'skkeleton',
-      doComplete: async (option) => {
-        const items = await getCompletionItems();
-        return { ...items };
-      },
-    })
+    languages.registerCompletionItemProvider(
+      'skkeleton',
+      'skk',
+      null,
+      {
+        provideCompletionItems: async (document: TextDocument, position: Position, _token: CancellationToken, _context: CompletionContext): Promise<CompletionList> => {
+          return await getCompletionItems(document, position);
+        },
+      })
   );
 };
 
-const getCompletionItems = async (): Promise<CompleteResult> => {
+const getCompletionItems = async (document: TextDocument, position: Position): Promise<CompletionList> => {
+  const offset = document.offsetAt(position);
   const candidates = (await workspace.nvim.call('denops#request', ['skkeleton', 'getCandidates', []])) as Array<
     [string, Array<string>]
   >;
-
-  const items: Array<VimCompleteItem> = candidates.flatMap((candidate) => {
+  const preEdit= (await workspace.nvim.call('denops#request', ['skkeleton', 'getPreEdit', []])) as string;
+  const range: Range = { start: document.positionAt(offset - preEdit.length), end: position };
+  const items = candidates.flatMap((candidate) => {
     const [kana, word] = candidate;
     return word
       .filter((word) => word.trim() !== '')
-      .map((word) => ({
-        word: `${kana}`,
-        menu: '[skkeleton]',
-        info: `${kana}\n${word}`,
-        abbr: word,
+      .map((word): CompletionItem => ({
+        label: word,
+        sortText: preEdit,
+        filterText: preEdit,
+        textEdit: { range, newText: word }
       }));
   });
 
   return {
     items,
+    isIncomplete: false,
   };
 };
